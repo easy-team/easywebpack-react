c4eac6f12e7634538d0cf82a5738bf1c57f26f52
hubcarl <hubcarl@126.com>
2017-12-13 17:38:20 +0800

    fix: adapter easywebpack 3.5.0 dll

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ exports.EasyWebpack = EasyWebpack;
 Object.assign(exports, EasyWebpack);
 exports.WebpackClientBuilder = require('./lib/client');
 exports.WebpackServerBuilder = require('./lib/server');
-exports.getWebpackConfig = config => {
-  return EasyWebpack.WebpackBuilder.getWebpackConfig(config, [exports.WebpackClientBuilder, exports.WebpackServerBuilder]);
+exports.getWebpackConfig = (config, option) => {
+  return EasyWebpack.WebpackBuilder.getWebpackConfig(config, [exports.WebpackClientBuilder, exports.WebpackServerBuilder], option);
 };
