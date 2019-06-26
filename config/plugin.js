e0aeb74eb7118fdd87a0396b9c0180b567df7eb7
sky <cl98914@alibaba-inc.com>
2019-06-26 16:52:51 +0800

    fix: rename dynamic plugin label name

--- config/plugin.js
+++ config/plugin.js
@@ -1,6 +1,6 @@
 'use strict';
 const ReactSSRDynamicChunkPlugin = require('./plugin/react-ssr-dynamic-chunk-webpack-plugin');
-exports.vuessrchunk = {
+exports.reactssrchunk = {
   type: ['server'],
   name: new ReactSSRDynamicChunkPlugin(),
   args: {
